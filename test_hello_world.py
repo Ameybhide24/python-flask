9b6d10ab2a6d98bddd4f189d75841a8cbaf31b0c
Amey Bhide <amey@Ameys-MacBook-Air.local>
2025-02-12 19:05:28 -0800

    add autofixture

--- test_hello_world.py
+++ test_hello_world.py
@@ -3,14 +3,17 @@ import unittest
 
 class TestHelloWorld(unittest.TestCase):
 
+    @pytest.fixture(autouse=True)
     def setUp(self):
         self.app = hello_world.app.test_client()
         self.app.testing = True
 
+    @pytest.fixture(autouse=True)
     def test_status_code(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
     
+    @pytest.fixture(autouse=True)
     def test_greeting_message(self):
         greeting = 'Welcome to CI/CD'
         self.assertEqual(hello_world.greet(), greeting)
